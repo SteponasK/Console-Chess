b471b24aaf116b21d549f57c1bcf7ccacb859618
SteponasK <132608893+SteponasK@users.noreply.github.com>
2023-11-01 14:50:53 +0200

    Added check to see if the rooks have moved

--- main.cpp
+++ main.cpp
@@ -21,11 +21,20 @@ extern bool white_mated = 0;
 extern bool black_mated = 0;
 extern bool draw = 0;
 extern int perpetualCheckCount = 0;
+
+extern int whiteARookMoved = 0;
+extern int whiteHRookMoved = 0;
+extern int blackARookMoved = 0;
+extern int blackHRookMoved = 0;
+extern int blackKingMoved = 0;
+extern int whiteKingMoved = 0;
 //int board[144];
 std::vector<Square_pair> generateMoves(int board[10 * 12], int colour);
 unsigned long long Perft(int depth, int board[120], int colour);
 
-
+void updateCastlingLegality(std::vector<boardState> boardStates, int& whiteARookMoved,
+    int& whiteHRookMoved, int& blackARookMoved, int& blackHRookMoved, int& blackKingMoved,
+    int& whiteKingMoved);
 int main() {
     Square_pair square_pair;
     std::vector<boardState> boardStates;
@@ -48,6 +57,7 @@ int main() {
         if (handleMove(square_pair, board, whiteTurn)) {
             whiteTurn = (whiteTurn ? 0 : 1); 
             addBoardState(boardStates, board);
+           // updateCastlingLegality(addBoardState,);
         }
         else std::cout << "handleMove returned 1: incorrect move played\n";
         // Choose piece
@@ -64,6 +74,53 @@ int main() {
         system("CLS");
     }
 	return 0;
+}
+void updateCastlingLegality(std::vector<boardState> boardStates, int& whiteARookMoved,
+    int& whiteHRookMoved, int& blackARookMoved, int& blackHRookMoved, int& blackKingMoved,
+    int& whiteKingMoved) {
+
+    if (boardStates.size() < 2)
+        return;
+    
+    //White Rook moved check
+    boardState currentBoard = boardStates.back();
+    auto previousIterator = boardStates.end() - 1;
+    boardState previousBoard = *previousIterator;
+    int bRooka = 21;
+    int bRookh = 28;
+    int wRooka = 91;
+    int wRookh = 98;
+    // If currently rooks are not in their default squares
+    if (currentBoard.array[bRooka] != -2) { 
+        blackARookMoved = true;
+    }
+    if (currentBoard.array[bRookh] != -2) {
+        blackHRookMoved = true;
+    }
+    if (currentBoard.array[wRooka] != 2) {
+        whiteARookMoved = true;
+    }
+    if (currentBoard.array[wRookh] != 2) {
+        whiteHRookMoved = true;
+    }
+    // If on the last move rooks were not in their squares
+    if (previousBoard.array[bRooka] != -2) {
+        blackARookMoved = true;
+    }
+    if (previousBoard.array[bRookh] != -2) {
+        blackHRookMoved = true;
+    }
+    if (previousBoard.array[wRooka] != 2) {
+        whiteARookMoved = true;
+    }
+    if (previousBoard.array[wRookh] != 2) {
+        whiteHRookMoved = true;
+    }
+
+    // 4 rook pos
+    // jeigu ne toj vietoj -> tada moved = true
+    // jeigu last lentoj buvo toj pos, bet current lentoj ne -> tada moved = true
+
 }
 unsigned long long Perft(int depth, int board[120], int colour) {
     int newBoard[120];
