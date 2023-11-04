1ca85aecf8361f509ab5cd0ea0df9babc820d07c
SteponasK <132608893+SteponasK@users.noreply.github.com>
2023-11-04 23:13:21 +0200

    adding countPosition function

--- evaluate.cpp
+++ evaluate.cpp
@@ -1,9 +1,24 @@
 #include "evaluate.h"
+#include "activityBoards.h"
+int evaluate(const std::array<int, 120>& board, const int colour) {
+	int whiteMaterial = countMaterial(board, 1); // count material
+	int blackMaterial = countMaterial(board,-1);
+
+	int whiteEval = whiteMaterial;
+	int blackEval = blackMaterial;
+	int evaluation = whiteEval - blackEval;
+	int perspective = (colour == 1) ? 1 : -1;
+	return evaluation * perspective;
+}
+int countPosition(const std::array<int, 120>& board, const int colour) {
+
+	return 1;
+}
 int countMaterial(const std::array<int, 120>& board, const int colour) {
-	 int points{};
-	 const int pieceValues[] = { 0, 100, 500, 300, 300, 900, 0 };// King score is not counted 
+	int points{};
+	const int pieceValues[] = { 0, 100, 500, 300, 300, 900, 0 };// King score is not counted 
 	for (int i = 21; i < 100; ++i) {
-		int value = board[i] * colour; 
+		int value = board[i] * colour;
 		if (value > 0) { // same Colour
 			if (value < 8)
 				points += pieceValues[value];
@@ -11,11 +26,3 @@ int countMaterial(const std::array<int, 120>& board, const int colour) {
 	}
 	return points;
 }
-int evaluate(const std::array<int, 120>& board, const int colour) {
-	int whiteEval = countMaterial(board, 1);
-	int blackEval = countMaterial(board,-1);
-
-	int evaluation = whiteEval - blackEval;
-	int perspective = (colour == 1) ? 1 : -1;
-	return evaluation * perspective;
-} 
